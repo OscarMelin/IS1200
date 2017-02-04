f1eba89b14ca64e92438e6f2a903944898dbceeb
Oscar Melin <oscarme@kth.se>
2017-02-04 01:58:15 +0100

    q2 done

--- Lab2 - C Programming/print-primes.c
+++ Lab2 - C Programming/print-primes.c
@@ -15,17 +15,13 @@ void print_number(int n){
 }
 
 void print_primes(int n){
-	// Should print out all prime numbers less than 'n'
-	// with the following formatting. Note that
-	// the number of columns is stated in the define
-	// COLUMNS
 
 	int i;
 
 	for (i = 1; i <= n; i++) {
 
-	if (is_prime(i))
-		print_number(i);
+		if (is_prime(i))
+			print_number(i);
 	}
 
 	printf("\n");
