556995937566aaab8169f98bb5f13caa30bb4421
Oscar Melin <oscarme@kth.se>
2017-02-04 01:55:40 +0100

    q2 done

--- Lab2 - C Programming/print-primes.c
+++ Lab2 - C Programming/print-primes.c
@@ -1,45 +1,56 @@
-/*
- print-prime.c
- By David Broman.
- Last modified: 2015-09-15
- This file is in the public domain.
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COLUMNS 6
+#define COLUMNS 3
 
+int count; // Global count for print_number
 
-void print_primes(int n){
-  // Should print out all prime numbers less than 'n'
-  // with the following formatting. Note that
-  // the number of columns is stated in the define
-  // COLUMNS
+void print_number(int n){
 
-  printf("%10d ", 2);
-  printf("%10d ", 3);
-  printf("%10d ", 5);
-  printf("%10d ", 7);
-  printf("%10d ", 11);
-  printf("%10d ", 13);
-  printf("\n");
-  printf("%10d ", 17);
-  printf("%10d ", 19);
-
-  printf("\n");
+	if (count%COLUMNS == 0 && count != 0)
+		printf("\n");
+  
+	printf("%10d ", n);
+	count++;
 }
 
+void print_primes(int n){
+	// Should print out all prime numbers less than 'n'
+	// with the following formatting. Note that
+	// the number of columns is stated in the define
+	// COLUMNS
+
+	int i;
+
+	for (i = 1; i <= n; i++) {
+
+	if (is_prime(i))
+		print_number(i);
+	}
+
+	printf("\n");
+}
+
+int is_prime(int n){
+
+	int i;
+
+	if (n == 1)
+		return 0;
+
+	for (i = 2; i <= n/2; i++) {		
+		if (n%i == 0)
+			return 0;
+	}
+	return 1;
+}
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
 int main(int argc, char *argv[]){
-  if(argc == 2)
-    print_primes(atoi(argv[1]));
-  else
-    printf("Please state an interger number.\n");
-  return 0;
+	if(argc == 2)
+		print_primes(atoi(argv[1]));
+	else
+		printf("Please state an interger number.\n");
+	return 0;
 }
-
- 
