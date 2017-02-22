7ebf6fc9fc416bdf73fe77a8ad921ac1e9fa58f2
Oscar Melin <oscarme@kth.se>
2017-02-23 00:08:24 +0100

    factorial works for all but 1 and 0

--- Lab4 - Processor Design/factorial.asm
+++ Lab4 - Processor Design/factorial.asm
@@ -1,25 +1,35 @@
 
-addi $a0, $0, 1		# Starting value of n
-addi $a1, $a0, 1	# $a1, number of multiplications
-#addi $a2, $0, 0		# $a2, i
-addi $v0, $0, 1		# result, $v0
+addi $v0, $0, 0		# Return register
+addi $a0, $0, 1		# n
+addi $a1, $0, 1		# i
+addi $v1, $0, 0		# return for multiply
+addi $a2, $v1, 1
 
 
 loop:
-	beq $a1, $0, end
-	addi $a1, $a1, -1
-	
-	addi $v1, $0, 0 	# $v1, result of multiplication
-	addi $a3, $0, 0		# $a3, j
+	beq  $a1, $a0, end
+	add  $0, $0, $0	# NOP
+
+	add $a2, $a2, $v1
+	addi $a3, $a1, 1
+	addi $v1, $0, 0
+	# Multiplies $a2 with $a3 and returns in $v1
 	multiply:
-		beq $a3, $a1, done
-		addi $a3, $a3, 1 	# j++
-		add $v1, $v1, $a1
-		beq $0, $0, multiply
+		beq  $a2, $0, done
+		add  $0, $0, $0	# NOP
+		
+		add	 $v1, $v1, $a3
+		addi $a2, $a2, -1
+
+		beq  $0, $0, multiply
+		add  $0, $0, $0	# NOP
 	
 	done:
-		addi $v0, $v1, 0
-		beq $0, $0, loop
+		addi $a1, $a1, 1	# i++
+		
+		beq  $0, $0, loop
+		add  $0, $0, $0		# NOP
 
 end:
-	add  $0,$0,$0	  # NOP
+	add  $v0, $v0, $v1	# Increment global return
+	beq  $0,$0,end
